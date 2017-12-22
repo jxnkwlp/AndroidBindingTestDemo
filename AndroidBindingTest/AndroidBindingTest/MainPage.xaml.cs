7f0893771212477a0e2705b9ab85fc1ab49ca780
jxnkwlp <jxnkwlp@hotmail.com>
2017-12-22 11:44:45 +0800

    添加项目文件。

---
+++ AndroidBindingTest/AndroidBindingTest/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidBindingTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            UserDialogs.Instance.Toast("这是提示~~~");
+
+        }
+
+    }
+}
